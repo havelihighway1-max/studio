b5954af7cbf646b86b3613697273c93b6a5a42c7
Haveli Highway <havelihighway1@gmail.com>
2025-10-08 00:31:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-915658214-98f6b",
+  "appId": "1:1006384574373:web:d1ac4d1e16234dac88424c",
+  "apiKey": "AIzaSyCERtAy2PqLRky0dtsWEY8brgwqbbJ8NTc",
+  "authDomain": "studio-915658214-98f6b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1006384574373"
+};
