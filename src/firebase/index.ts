ce117e7fa382c1fdbbba3b32d494f3010ff2a407
Haveli Highway <havelihighway1@gmail.com>
2025-10-13 07:11:24 +0000

    import { initializeApp } from "firebase/app"; import { getPerformance }

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,7 +3,8 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
+import { getPerformance } from 'firebase/performance';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -25,11 +26,20 @@ export function initializeFirebase() {
       firebaseApp = initializeApp(firebaseConfig);
     }
 
+    // Initialize Performance Monitoring
+    if (typeof window !== "undefined") {
+        getPerformance(firebaseApp);
+    }
+
     return getSdks(firebaseApp);
   }
 
   // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  const firebaseApp = getApp();
+   if (typeof window !== "undefined") {
+      getPerformance(firebaseApp);
+   }
+  return getSdks(firebaseApp);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
